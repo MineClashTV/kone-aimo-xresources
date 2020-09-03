7769c8738e4847f35badf9769501993a0f9d9be1
Pascal Puffke <pascalpuffke@protonmail.ch>
2020-09-03 18:49:53 +0200

    forgot to remove my bad logger from this file
    im dumb

--- src/de/mineclashtv/ArgumentParser.java
+++ src/de/mineclashtv/ArgumentParser.java
@@ -2,7 +2,6 @@ package de.mineclashtv;
 
 public class ArgumentParser {
 
-    private static final Logger logger = new Logger();
     private static final String ls = System.lineSeparator();
 
     public static void doShit(String[] args) {
@@ -11,7 +10,7 @@ public class ArgumentParser {
                 case "--dev": Main.dev = args[i + 1]; break;
                 case "--primary": Main.primary = Integer.parseInt(args[i + 1]); break;
                 case "--secondary": Main.secondary = Integer.parseInt(args[i + 1]); break;
-                case "--help": logger.log(
+                case "--help": System.out.println(
                         "This program sets the colors of a Roccat Kone Aimo mouse to your Xresources." + ls +
                                 "Options: " + ls +
                                 "--dev          Device to use, e.g. /dev/hidraw2" + ls +
